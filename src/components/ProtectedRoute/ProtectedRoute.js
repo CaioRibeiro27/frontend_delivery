5a49c39909f6545a9b8729e3ebca998349e4b400
CaioRibeiro <caiopr0102@gmail.com>
2025-12-09 22:52:12 -0300

    bug dashboard v2

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
-  const storedUser = localStorage.getItem("user");
+  const storedUser = sessionStorage.getItem("user");
 
   //Lógica do Segurança:
   if (!storedUser) {
